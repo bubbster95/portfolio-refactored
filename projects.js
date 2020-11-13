90130086ad0b36fad68f33c1856fc6312e7a9d97
bubbster95 <stiles.billy@gmail.com>
2020-11-13 16:32:38 -0500

    Menu fully functional, Projects populate from an object, Css Coming along

---
+++ projects.js
@@ -0,0 +1,270 @@
+const objArr = {
+    bouquet: {
+        languages: ['React.js', 'JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Bouq-A-Okay',
+            url: 'assets/Thumbnails/Bouquet.gif',
+            description: 'React.js / JavaScript',
+            alt: 'Bouq-A-Okay thumbnail.'
+        },
+        images:[
+            ['assets/Gallery/Bouq-a-okay/BouqHomePage-min.png',
+            'Bouq-A-Okay home page for desktop.',
+            'https://bouq-a-okay.netlify.app/'
+            ],
+            ['assets/Gallery/Bouq-a-okay/BouqHomeMobile-min.png',
+            'Cart page of Bouq-A-Okay',
+            'https://bouq-a-okay.netlify.app/'
+            ],
+            ['assets/Gallery/Bouq-a-okay/ChooseABouq-min.png',
+            'Choose a Bouquet page.',
+            'https://bouq-a-okay.netlify.app/'
+            ],
+            ['assets/Gallery/Bouq-a-okay/PickYOurPallet-min.png',
+            'Pick your pallet section of the Design a Bouquet page.',
+            'https://bouq-a-okay.netlify.app/'
+            ],
+            ['assets/Gallery/Bouq-a-okay/PickYourFlowers-min.png',
+            'Pick your flowers section of the Design a Bouquet page.',
+            'https://bouq-a-okay.netlify.app/'
+            ]
+        ]
+    },
+    disco: {
+        languages: ['JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Disco Dude',
+            url: 'assets/Thumbnails/DiscoGif.gif',
+            description: 'JavaScript / Animation',
+            alt: 'Disco Dude thumbnail'
+        }, 
+        images:[
+            ['assets/Gallery/DiscoDude/Target.gif',
+            'Gif of dude dancing',
+            'https://disco-dude.netlify.app'
+            ],
+            ['assets/Gallery/DiscoDude/Target.gif',
+            'Gif of four targets',
+            'https://disco-dude.netlify.app'
+            ],
+            ['assets/Gallery/DiscoDude/DiscoFloor.gif',
+            'Gif of a disco floor',
+            'https://disco-dude.netlify.app'
+            ],
+            ['assets/Gallery/DiscoDude/DiscoDudeOnStage.png',
+            'Screen shot of Disco Dude',
+            'https://disco-dude.netlify.app'
+            ]
+        ]
+    },
+    turner: {
+        languages: ['JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Wood Turner\'s Calculator',
+            url: 'assets/Thumbnails/TurnersGif.gif',
+            description: 'JavaScript / Design',
+            alt: 'Wood Turners Calculator thumbnail.'
+        },
+        images:[
+            ['assets/Gallery/turners-calculator/WebVase.png',
+            'Screen shot of Turner\'s Calculator web browser layout',
+            'https://wood-turners-calculator.netlify.app/'
+            ],
+            ['assets/Gallery/turners-calculator/VaseMobile.png',
+            'Screen shot of Turner\'s Calculator mobile browser layout',
+            'https://wood-turners-calculator.netlify.app/'
+            ],
+            ['assets/Gallery/turners-calculator/WebBluePrints.png',
+            'Screen shot of blueprints',
+            'https://wood-turners-calculator.netlify.app/'
+            ],
+            ['assets/Gallery/turners-calculator/PrintPreview.png',
+            'Screen shot of Trapezoides print preview',
+            'https://wood-turners-calculator.netlify.app/'
+            ]
+        ]
+    },
+    scroll: {
+        languages: ['React.js', 'JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Infinite Scroll React',
+            url: 'assets/Thumbnails/InfiniteScroll.gif',
+            description: 'React.js / JavaScript',
+            alt: 'Infinite Scroll React thumbnail.'
+        },
+        images:[
+            ['assets/Gallery/InfiniteScroll/InfiniteScroll.png',
+            'Screen Shot of Infinite Scroll landng page',
+            'https://wills-infinite-scroll.netlify.app/'
+            ],
+            ['assets/Gallery/InfiniteScroll/Profile 1.png',
+            'Screen Shot of Infinite Scroll avatar 1',
+            'https://wills-infinite-scroll.netlify.app/'
+            ],
+            ['assets/Gallery/InfiniteScroll/Profile2.png',
+            'Screen Shot of Infinite Scroll avatar 2',
+            'https://wills-infinite-scroll.netlify.app/'
+            ]
+        ]
+    },
+    portfolio: {
+        languages: ['JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Old Portfolio Site',
+            url: 'assets/Thumbnails/PortfolioGif.gif', 
+            description: 'JavaScript / Design',
+            alt: 'Old Portfolio Site thumbnail.'
+        },
+        images:[
+            ['assets/Gallery/PortfolioSite/WillStilesSquare.png',
+            'Portfolio Site Screen Shot'
+            ],
+            ['assets/Gallery/PortfolioSite/MobileFirst.png',
+            'Mobile portfolio site'
+            ],
+            ['assets/Gallery/PortfolioSite/PanelWeb.png',
+            'Responsive panel'
+            ],
+            ['assets/Gallery/PortfolioSite/MobilePanel.png',
+            'Mobile panel'
+            ],
+            ['assets/Thumbnails/DiscoGif.gif',
+            'Disco Dude sillohette gif'
+            ],
+            ['assets/Gallery/Animation/WSLogo/WS9.png',
+            'WS logo gif'
+            ],
+            ['assets/Gallery/Background/Moon.png',
+            'Moon image'
+            ]
+        ]
+    },
+    motif: {
+        languages: ['JavaScript', 'Design'],
+        thumbnail: {
+            title: 'Motif Magazine',
+            url: 'assets/Gallery/Motif/FoodCover.jpg',
+            description: 'React.js / JavaScript',
+            alt: 'Motif Magazine portfolio thumbnail.'
+        }, 
+        images:[
+            ['Food Issue Cover',
+            'assets/Gallery/Motif/FoodCover.jpg',
+            'Motif Magazine employs me as a freelance designer and Wordpress developer. I designed this Issue of Motif during my time as lead designer. All of these images will take you to an “Id Publish Online magazine”. Keep in mind that this feature produces unfortunate design defects not represented in the final print of each issue.',
+            'Motif Food Issue Cover',
+            'https://indd.adobe.com/view/9c26a979-cf26-45f9-96a9-9fcfd267d14d'
+            ],
+            ['Cannabis Issue, Eating Spread',
+            'assets/Gallery/Motif/CannabisEating.jpg',
+            'The Eating section in Motif, requires a special visual treatment. This page shows a technique I devised to allow more image space and text by making half of the image a screen back. To see more and read the issue click the image above.',
+            'Cannabis Issue Eating Spread',
+            'https://indd.adobe.com/view/8720e976-0198-4b0b-9893-742b5e699749'
+            ],
+            ['Tiny Lobster Article',
+            'assets/Gallery/Motif/TinyLobster.jpg',
+            'While I was the lead designer, each issue had a back page devoted to telling a story using images. In this issue, I wrote an article about creating the cover with a local artist. Read the issue by clicking the image above.',
+            'Tiny Lobster back story',
+            'https://indd.adobe.com/view/9c26a979-cf26-45f9-96a9-9fcfd267d14d'
+            ],
+            ['A Taste of Ireland',
+            'assets/Gallery/Motif/Centerspread.jpg',
+            'For each issue, I designed a unique centerspread graphic spanning two pages. This centerspread focuses on the beauty of Ireland as told by the native Irish author through his photography. View this centerspread by clicking the image above.',
+            'Motif Spring guide centerspread',
+            'https://indd.adobe.com/view/8a081028-feae-4e76-9c7b-81c4ba0f124a'
+            ],
+            ['A Taste of Antarctica',
+            'assets/Gallery/Motif/AntarcticaSpread.jpg',
+            'This centerspread highlights the beauty of Antarctica while supporting the narrative about climate change and its effect on the fragile ecosystem of Antarctica.',
+            'Motif Spring guide centerspread',
+            'https://indd.adobe.com/view/f4444bf0-704e-4840-88c5-e25f223a1cf7'
+            ],
+            ['Hot Rods Photo Bash',
+            'assets/Gallery/Motif/HotRods.jpg',
+            'To close out each issue of Motif I created a photo bash from a folder of provided photos. The challenge with this page was filtering through a couple hundred photos of hot rods from a local car show.',
+            'Hot Rod Photo Bash',
+            'https://indd.adobe.com/view/81200c61-0434-4e1b-b15d-30dcbc61a7b0'
+            ],
+            ['Whale Guitar Photo Bash',
+            'assets/Gallery/Motif/WhaleGuitar.jpg',
+            'This photo bash tells the story of a local piece of art that comes in the form of both a guitar and a whale.  After choosing this hero image it occurred to me how tough it is to silhouette a guitar. With a little time and patience, anything is possible.',
+            'Whale Guitar Photo Bash',
+            'https://indd.adobe.com/view/f4444bf0-704e-4840-88c5-e25f223a1cf7'
+            ]
+        ]
+    },
+    khoo: {
+        languages: ['Design'],
+        thumbnail: {
+            title: 'Khoo Design',
+            url: 'assets/Thumbnails/KhooStyle.png',
+            description: 'Design',
+            alt: 'Khoo Design thumbnail'
+            }, 
+        images:[
+            ['assets/Gallery/Khoo/SundayKhoo.jpg',
+            'Sunday Khoo table tent'
+            ],
+            ['assets/Gallery/Khoo/ThirstyThursday.jpg',
+            'Thirsty Thursday table tent'
+            ],
+            ['assets/Gallery/Khoo/DinningMenuGray2.jpg',
+            'Khoo Menu pg 2'
+            ],
+            ['assets/Gallery/Khoo/DinningMenuGray5.jpg',
+            'Khoo Menu pg 5'
+            ],
+            ['assets/Gallery/Khoo/DinningMenuGray7.jpg',
+            'Khoo Sushi menu'
+            ],
+            ['assets/Gallery/Khoo/DrinksGray1.jpg',
+            'Khoo Special drinks menu'
+            ],
+            ['assets/Gallery/Khoo/DrinksGray2.jpg',
+            'Khoo drinks menu'
+            ]
+        ]
+    },
+    animation: {
+        languages: ['Design'],
+        thumbnail: {
+            title: 'Animation',
+            url: 'assets/Thumbnails/disco-dude.gif',
+            description: 'Animation',
+            alt: 'Animation Thumbnail'
+            }, 
+        images:[
+            ['assets/Gallery/DiscoDude/disco-dude.gif',
+            'Gif of a Disco Dancer',
+            'https://disco-dude.netlify.app'
+            ],
+            ['assets/Gallery/Animation/Venga Climb.mp4',
+            'Venda Climbing comercial'
+            ],
+            ['assets/Gallery/Animation/Zakatana.mp4',
+            'Zackatana trailer'
+            ],
+            ['assets/Gallery/Animation/AvacadoRun.mp4',
+            'Avacoda Run Cycle'
+            ]
+        ]
+    },
+};
+
+populate = () => {
+    let projects = document.querySelector('.filtered-projects');
+    let objKeys = Object.keys(objArr);
+    console.log('keys', objKeys);
+
+    objKeys.map(project => {
+        let projectWrapper = document.createElement('DIV');
+        projectWrapper.className = 'project-wrapper';
+        projectWrapper.id = project;
+        projects.appendChild(projectWrapper);
+
+        let projectImage = document.createElement('Div');
+        projectImage.className = 'project-image';
+        projectImage.style.backgroundImage = `url(${objArr[project].thumbnail.url})`;
+        projectImage.alt = `${objArr[project].thumbnail.alt}`
+        projectWrapper.appendChild(projectImage)
+    })
+}
