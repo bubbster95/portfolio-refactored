da32946d577d2c229340c637967d88a005e6e690
bubbster95 <stiles.billy@gmail.com>
2020-11-15 13:25:43 -0500

    Fixed project tile hover bug, tweaked css inside modals

--- projects.js
+++ projects.js
@@ -57,7 +57,11 @@ const objArr = {
             gitHub: 'https://github.com/bubbster95/DiscoDude'
         },
         images:[
-            ['assets/Gallery/DiscoDude/Target.gif',
+            ['assets/Gallery/DiscoDude/DiscoDudeOnStage.png',
+            'Screen shot of Disco Dude',
+            'https://disco-dude.netlify.app'
+            ],
+            ['assets/Gallery/DiscoDude/disco-dude.gif',
             'Gif of dude dancing',
             'https://disco-dude.netlify.app'
             ],
@@ -67,10 +71,6 @@ const objArr = {
             ],
             ['assets/Gallery/DiscoDude/DiscoFloor.gif',
             'Gif of a disco floor',
-            'https://disco-dude.netlify.app'
-            ],
-            ['assets/Gallery/DiscoDude/DiscoDudeOnStage.png',
-            'Screen shot of Disco Dude',
             'https://disco-dude.netlify.app'
             ]
         ]
@@ -175,12 +175,6 @@ const objArr = {
             ],
             ['assets/Thumbnails/DiscoGif.gif',
             'Disco Dude sillohette gif'
-            ],
-            ['assets/Gallery/Animation/WSLogo/WS9.png',
-            'WS logo gif'
-            ],
-            ['assets/Gallery/Background/Moon.png',
-            'Moon image'
             ]
         ]
     },
@@ -200,45 +194,31 @@ const objArr = {
             The challenge now is to merge my knowlege of programming and design to work with wordpress sites. `
         }, 
         images:[
-            ['Food Issue Cover',
-            'assets/Gallery/Motif/FoodCover.jpg',
-            'Motif Magazine employs me as a freelance designer and Wordpress developer. I designed this Issue of Motif during my time as lead designer. All of these images will take you to an “Id Publish Online magazine”. Keep in mind that this feature produces unfortunate design defects not represented in the final print of each issue.',
+            ['assets/Gallery/Motif/FoodCover.jpg',
             'Motif Food Issue Cover',
             'https://indd.adobe.com/view/9c26a979-cf26-45f9-96a9-9fcfd267d14d'
             ],
-            ['Cannabis Issue, Eating Spread',
-            'assets/Gallery/Motif/CannabisEating.jpg',
-            'The Eating section in Motif, requires a special visual treatment. This page shows a technique I devised to allow more image space and text by making half of the image a screen back. To see more and read the issue click the image above.',
+            ['assets/Gallery/Motif/CannabisEating.jpg',
             'Cannabis Issue Eating Spread',
             'https://indd.adobe.com/view/8720e976-0198-4b0b-9893-742b5e699749'
             ],
-            ['Tiny Lobster Article',
-            'assets/Gallery/Motif/TinyLobster.jpg',
-            'While I was the lead designer, each issue had a back page devoted to telling a story using images. In this issue, I wrote an article about creating the cover with a local artist. Read the issue by clicking the image above.',
+            ['assets/Gallery/Motif/TinyLobster.jpg',
             'Tiny Lobster back story',
             'https://indd.adobe.com/view/9c26a979-cf26-45f9-96a9-9fcfd267d14d'
             ],
-            ['A Taste of Ireland',
-            'assets/Gallery/Motif/Centerspread.jpg',
-            'For each issue, I designed a unique centerspread graphic spanning two pages. This centerspread focuses on the beauty of Ireland as told by the native Irish author through his photography. View this centerspread by clicking the image above.',
+            ['assets/Gallery/Motif/Centerspread.jpg',
             'Motif Spring guide centerspread',
             'https://indd.adobe.com/view/8a081028-feae-4e76-9c7b-81c4ba0f124a'
             ],
-            ['A Taste of Antarctica',
-            'assets/Gallery/Motif/AntarcticaSpread.jpg',
-            'This centerspread highlights the beauty of Antarctica while supporting the narrative about climate change and its effect on the fragile ecosystem of Antarctica.',
+            ['assets/Gallery/Motif/AntarcticaSpread.jpg',
             'Motif Spring guide centerspread',
             'https://indd.adobe.com/view/f4444bf0-704e-4840-88c5-e25f223a1cf7'
             ],
-            ['Hot Rods Photo Bash',
-            'assets/Gallery/Motif/HotRods.jpg',
-            'To close out each issue of Motif I created a photo bash from a folder of provided photos. The challenge with this page was filtering through a couple hundred photos of hot rods from a local car show.',
+            ['assets/Gallery/Motif/HotRods.jpg',
             'Hot Rod Photo Bash',
             'https://indd.adobe.com/view/81200c61-0434-4e1b-b15d-30dcbc61a7b0'
             ],
-            ['Whale Guitar Photo Bash',
-            'assets/Gallery/Motif/WhaleGuitar.jpg',
-            'This photo bash tells the story of a local piece of art that comes in the form of both a guitar and a whale.  After choosing this hero image it occurred to me how tough it is to silhouette a guitar. With a little time and patience, anything is possible.',
+            ['assets/Gallery/Motif/WhaleGuitar.jpg',
             'Whale Guitar Photo Bash',
             'https://indd.adobe.com/view/f4444bf0-704e-4840-88c5-e25f223a1cf7'
             ]
@@ -316,44 +296,53 @@ const objArr = {
     },
 };
 
+// Fills project section with project tiles
 populate = () => {
     let projects = document.querySelector('.filtered-projects');
     let objKeys = Object.keys(objArr);
 
+    // Create project tile for each object key
     objKeys.map(project => {
+        // Container
         let projectWrapper = document.createElement('DIV');
         projectWrapper.className = 'project-wrapper';
         projectWrapper.id = project;
         projects.appendChild(projectWrapper);
 
+        // Image
         let projectImage = document.createElement('Div');
         projectImage.className = 'project-image';
         projectImage.style.backgroundImage = `url(${objArr[project].thumbnail.src})`;
         projectImage.alt = `${objArr[project].thumbnail.alt}`;
         projectWrapper.appendChild(projectImage);
 
+        // Title
         let projectTitle = document.createElement('H3');
         projectTitle.className = 'project-title';
         projectTitle.innerHTML = `${objArr[project].thumbnail.title}`;
         projectWrapper.appendChild(projectTitle)
 
+        // Text
         let projectDescription = document.createElement('P');
         projectDescription.className = 'project-description';
         projectDescription.innerHTML = `${objArr[project].thumbnail.description}`;
         projectWrapper.appendChild(projectDescription);
 
+        // Button
         let viewProject = document.createElement('BUTTON');
         viewProject.className = 'view-project-button';
         viewProject.innerHTML = 'View Project';
         viewProject.addEventListener('click', () => {modal(project, projects)})
         projectWrapper.appendChild(viewProject);
 
+        // Hover Events
         let items = [projectWrapper, projectImage, projectTitle, viewProject];
         items.map(item => {item.addEventListener('mouseover', addVisibility)})
         projectWrapper.addEventListener('mouseout', removeVisibilty)
     })
 }
 
+// Hover over project tile
 addVisibility = (e) => {
     let parent = e.target.offsetParent
     let children = parent.children
@@ -363,6 +352,7 @@ addVisibility = (e) => {
     }
 }
 
+// Hover off project tile
 removeVisibilty = (e) => {
     let parent = e.target.offsetParent
     let children = parent.children
@@ -372,23 +362,28 @@ removeVisibilty = (e) => {
     }
 }
 
+// Populate / Toggle modal from object information
 modal = (project, projects) => {
+
+    // If modal exists toggle visibility
     if (document.getElementById(project + '-modal')) {
+        showSlides(slideIndex = 1, document.getElementById(project + '-modal'))
         document.getElementById(project + '-close-box').style.display = 'block';
         document.getElementById(project + '-modal').style.display = 'block';
-    } else {
+    } else { // Generate modal and fill with content
         let modal = document.createElement('DIV');
         modal.className = 'modal';
         modal.id = project + '-modal';
         projects.appendChild(modal);
 
-        // Close Modal
+        // Close Modal Button
         let closeBox = document.createElement('DIV');
         closeBox.className = 'close-box';
         closeBox.id = project + '-close-box';
         closeBox.addEventListener('click', () => {closeModal(project)})
         projects.appendChild(closeBox)
 
+        // Close by clicking outsite box
         let close = document.createElement('BUTTON');
         close.className = 'close-modal';
         close.innerHTML = 'X';
@@ -401,8 +396,9 @@ modal = (project, projects) => {
         carosel.className = 'carosel';
         modal.appendChild(carosel)
 
-        let images = objArr[project].images;
-        images.map((image, index) => {
+        // Loop thorugh available images
+        let modalImages = objArr[project].images;
+        modalImages.map((image, index) => {
             let newImage = document.createElement('IMG');
             newImage.className = 'modal-slide-image';
             newImage.src = image[0];
@@ -443,6 +439,7 @@ modal = (project, projects) => {
         summary.innerHTML = objArr[project].info.summary
         textBox.appendChild(summary)
 
+        // Create site link button if possible
         if (objArr[project].info.url){
             let viewSite = document.createElement('BUTTON');
             viewSite.className = 'modal-view-site';
@@ -450,6 +447,7 @@ modal = (project, projects) => {
             viewSite.addEventListener('click', () => { goToLink(objArr[project].info.url) });
             textBox.appendChild(viewSite);
         }
+        // Create Github link button if possible
         if (objArr[project].info.gitHub) {
             let veiwGit = document.createElement('BUTTON');
             veiwGit.className = 'modal-view-git';
@@ -457,8 +455,7 @@ modal = (project, projects) => {
             veiwGit.addEventListener('click', () => { goToLink(objArr[project].info.gitHub) });
             textBox.appendChild(veiwGit);
         }
-        
-        currentSlide(0, modal)
+        showSlides(slideIndex = 1, modal)
     }
 }
 
@@ -466,8 +463,6 @@ modal = (project, projects) => {
 let slideIndex = 1;
 
 plusSlide = (n, modal) => { showSlides(slideIndex += n, modal); }
-
-currentSlide = (n, modal) => { showSlides(slideIndex = n, modal); }
 
 showSlides = (n, modal) => {
     // Change Slides
@@ -480,7 +475,6 @@ showSlides = (n, modal) => {
     slides[slideIndex-1].className+= ' current-slide';
 }
 /* End modal Controls */
-
 
 closeModal = (project) => {
     document.getElementById(project + '-close-box').style.display = 'none';
